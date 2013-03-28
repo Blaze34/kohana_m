5d982d1e2cc7aa124c6955b0b2329e613133c7ad
sergey <squaremind@ukr.net>
2013-03-28 15:08:01 +0200

    holder model

---
+++ application/classes/model/holder.php
@@ -0,0 +1,29 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Model_Holder extends Jelly_Model implements Acl_Resource_Interface {
+
+    public static function initialize(Jelly_Meta $meta)
+	{
+		$meta->fields(array(
+			'id' => Jelly::field('primary'),
+            'title' => Jelly::field('string', array(
+                'rules' => array(
+                    array('not_empty')
+                ),
+                'unique' => TRUE,
+                'label' => 'holder.field.title'
+            )),
+            'body' => Jelly::field('text', array(
+                'rules' => array(
+                    array('not_empty')
+                ),
+                'label' => 'holder.field.body'
+            ))
+		));
+	}
+
+    public function get_resource_id()
+    {
+        return 'holder';
+    }
+}
