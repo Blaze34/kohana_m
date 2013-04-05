98e16c83ee2769117d6146df10afffd162a9dd3d
sergey <squaremind@ukr.net>
2013-04-05 16:11:35 +0300

    added novelty block on index

--- application/classes/controller/index.php
+++ application/classes/controller/index.php
@@ -4,11 +4,10 @@ class Controller_Index extends Controller_Web {
 
 	public function action_index()
 	{
-        list($polls, $comments, $materials) = $this->get_popular ();
+        $_popular = $this->get_popular ();
+        $_novelty = $this->get_novelty();
 
-        $this->view()->materials = $materials;
-        $this->view()->comments = $comments;
-        $this->view()->polls = $polls;
+        $this->view(array('_popular' => $_popular, '_novelty' => $_novelty));
 	}
 
     /**
@@ -61,6 +60,56 @@ class Controller_Index extends Controller_Web {
             }
         }
 
-        return array($polls, $comments, $materials);
+        return array('polls' => $polls, 'comments' =>  $comments, 'materials' => $materials);
+    }
+
+    protected function get_novelty ()
+    {
+        $comments = $polls = array();
+
+        $materials = Jelly::query ('material')->where('on_index', '=', 1)->order_by('novelty_index', 'DESC')->order_by('date', 'DESC')->pagination ('popular')->select_all();
+
+        $mids = array();
+
+        foreach ($materials as $m)
+        {
+            $mids[] = $m->id();
+        }
+
+        if ($materials)
+        {
+            $comments = Jelly::query ('comment')
+                ->with ('material')
+                ->select_column (array(array(DB::expr ('COUNT(comments.id)'), 'count')))
+                ->group_by ('material_id')
+                ->select_all ()->as_array ('material');
+
+            if (sizeof ($comments))
+            {
+                $polls_arr = Jelly::query ('poll')
+                    ->select_column (array(array(DB::expr ('COUNT(id)'), 'count'), 'type_id', 'value'))
+                    ->where ('type', '=', Jelly::factory ('material')->get_resource_id ())
+                    ->where ('type_id', 'IN', $mids)
+                    ->group_by ('type_id', 'value')
+                    ->select_all ()->as_array ();
+
+                if (sizeof ($polls_arr))
+                {
+                    foreach ($polls_arr as $item)
+                    {
+                        if ($item['value'])
+                        {
+                            $polls[$item['type_id']]['like'] = $item['count'];
+                        }
+                        else
+                        {
+                            $polls[$item['type_id']]['dislike'] = $item['count'];
+                        }
+                    }
+                }
+            }
+        }
+
+        return array('polls' => $polls, 'comments' =>  $comments, 'materials' => $materials);
     }
 } // End Welcome
--- application/views/index/index.php
+++ application/views/index/index.php
@@ -5,13 +5,44 @@
 <div class="span9 popular">
     <div class="block_title">Популярное</div>
     <div class="thum_items">
-        <?if (sizeof($materials)):?>
+        <?if (sizeof($_popular['materials'])):?>
             <ul class="thumbnails">
-                <?foreach ($materials as $m):?>
+                <?foreach ($_popular['materials'] as $m):?>
                     <?
-                    $comm = Arr::get($comments[$m->id()], 'count', 0);
-                    $like = Arr::get($polls[$m->id()], 'like', 0);
-                    $dislike = Arr::get($polls[$m->id()], 'dislike', 0)
+                    $comm = Arr::get($_popular['comments'][$m->id()], 'count', 0);
+                    $like = Arr::get($_popular['polls'][$m->id()], 'like', 0);
+                    $dislike = Arr::get($_popular['polls'][$m->id()], 'dislike', 0)
+                    ?>
+                    <li class="thum_item">
+                        <a href="<?=Route::url('default', array('controller' => 'material', 'action' => 'show', 'id' => $m->id()))?>" class="thumbnail">
+                            <img src="/<?=$m->thumb()?>" alt="<?=$m->title?>"/>
+                        </a>
+                        <?if($user AND $user->is_admin()):?>
+                            <div class="hide_from_page"><a href="<?=Route::url('default', array('controller' => 'material', 'action' => 'onindex', 'id' => $m->id()))?>"><i class="icon-remove"></i> Убрать с главной</a></div>
+                        <?endif;?>
+                        <div class="item_txt">
+                            <?=Text::limit_words($m->title, 15)?>
+                        </div>
+                        <div class="item_stat">Комм <?=$comm?>. &nbsp; <?=$like?><i class="icon-thumbs-up"></i>&nbsp;&nbsp; <?=$dislike?><i class="icon-thumbs-down"></i></div>
+                    </li>
+                <?endforeach;?>
+            </ul>
+            <?=isset($pagination) ? $pagination : ''?>
+        <?else : ?>
+            Список пуст
+        <?endif;?>
+    </div>
+</div>
+<div class="span12 block popular novelty">
+    <div class="block_title">Новинки</div>
+    <div class="thum_items">
+        <?if (sizeof($_novelty['materials'])):?>
+            <ul class="thumbnails">
+                <?foreach ($_novelty['materials'] as $m):?>
+                    <?
+                    $comm = Arr::get($_novelty['comments'][$m->id()], 'count', 0);
+                    $like = Arr::get($_novelty['polls'][$m->id()], 'like', 0);
+                    $dislike = Arr::get($_novelty['polls'][$m->id()], 'dislike', 0)
                     ?>
                     <li class="thum_item">
                         <a href="<?=Route::url('default', array('controller' => 'material', 'action' => 'show', 'id' => $m->id()))?>" class="thumbnail">
